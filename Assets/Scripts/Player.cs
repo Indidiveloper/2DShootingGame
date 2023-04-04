ccffb297d0fd735b7232931d9e8f3d46e1d68877
MINUK <zoow2@naver.com>
2023-04-04 21:04:30 +0900

    Enemy Spawn Point 추가, Enemy 투사체 발사 구현, Player 피격이벤트 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,6 +17,8 @@ public class Player : MonoBehaviour
     public GameObject bulletObjA;
     public GameObject bulletObjB;
 
+    public GameManager manager;
+
     Animator anim;
 
     private void Awake()
@@ -119,6 +121,11 @@ public class Player : MonoBehaviour
                     isTouchLeft = true;
                     break;
             }
+        }
+        else if(collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "EnemyBullet")
+        {
+            manager.RespawnPlayer();
+            gameObject.SetActive(false);            
         }
     }
 
